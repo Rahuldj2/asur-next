79dc458d879e1622d5b161f51db07c6a20f19db4
Rahuldj <rj712@snu.edu.in>
2024-08-09 12:55:32 +0530

    db connection pool created

--- src/pages/api/db.js
+++ src/pages/api/db.js
@@ -1,14 +1,14 @@
-// db.js
 import mysql from 'mysql';
 
-const connection = mysql.createConnection({
-  host:process.env.NEXT_PUBLIC_AWS_DB_URL,
+const pool = mysql.createPool({
+  connectionLimit: 10, // Maximum number of connections in the pool
+  host: process.env.NEXT_PUBLIC_AWS_DB_URL,
   user: process.env.NEXT_PUBLIC_AWS_DB_USER,
   password: process.env.NEXT_PUBLIC_AWS_DB_PASSWORD,
   database: process.env.NEXT_PUBLIC_AWS_DB_NAME,
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Database connection error: ' + err.stack);
     return;
@@ -17,6 +17,7 @@ connection.connect((err) => {
   console.log('Host:', connection.config.host);
   console.log('User:', connection.config.user);
   console.log('Database:', connection.config.database);
+  connection.release(); // Release the connection back to the pool
 });
 
-export default connection;
+export default pool;
